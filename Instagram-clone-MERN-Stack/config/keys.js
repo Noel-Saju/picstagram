df437767387cd43c889456602363c1fe1e5d660a
Noel_Saju <72462146+Noel-jediknight@users.noreply.github.com>
2021-12-08 22:53:25 +0530

    godspeed

---
+++ Instagram-clone-MERN-Stack/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://admin:admin@cluster0.plk2g.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "sdfahksdjfh",
+  SENDGRID_API: process.env.SENDGRID_API,
+  EMAIL: process.env.EMAIL,
+};
